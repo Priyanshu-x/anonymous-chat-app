c41adb4b21a57499d0d9284d6b54fb0aec073a3d
Priyanshu-x <priyanshu18181@gmail.com>
2025-10-02 15:17:50 +0530

    Images sharing feature started working

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-	content: { type: String },
-	type: { type: String, enum: ['text', 'image', 'voice'], default: 'text' },
-	imageUrl: { type: String },
-	voiceUrl: { type: String },
-	isPinned: { type: Boolean, default: false },
-	createdAt: { type: Date, default: Date.now }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String },
+  type: { type: String, enum: ['text', 'image', 'voice'], default: 'text' },
+  fileUrl: { type: String },
+  fileName: { type: String },
+  isPinned: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
